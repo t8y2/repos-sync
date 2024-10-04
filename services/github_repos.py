ec4f9010a66fc7875a3084be593895315837faef
skylertong <skyler@transcengram.com>
2024-10-04 23:58:35 +0800

    feat: ✨  完成github仓库检测及创建

---
+++ services/github_repos.py
@@ -0,0 +1,54 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+class GitHubRepoManager:
+    @classmethod
+    def __init__(cls):
+        # 加载 .env 文件
+        load_dotenv()
+        # 获取环境变量值
+
+        cls.token = os.getenv('GITHUB_TOKEN')
+        cls.headers = {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': f'Bearer {cls.token}',
+        }
+        cls.github_repos_get_url_prefix = "https://api.github.com/repos/{owner}/{repo}"
+        cls.github_repos_create_url = "https://api.github.com/user/repos"
+
+    @classmethod
+    def repo_exists(cls, dest_username: str, dest_repo_name: str) -> bool:
+
+        repo_url = cls.github_repos_get_url_prefix.format(owner=dest_username, repo=dest_repo_name)
+        response = requests.get(repo_url, headers=cls.headers)
+        if response.status_code == 200:
+            print(f"查询到Github仓库 '{dest_repo_name}' 已经存在。")
+            return True
+        return False
+
+    @classmethod
+    def create_repo(cls, dest_username: str, dest_repo_name: str, private=True):
+        """创建新的 GitHub 仓库。"""
+        if cls.repo_exists(dest_username, dest_repo_name):
+            print(f"仓库 '{dest_repo_name}' 已经存在, 跳过创建步骤。")
+            return
+        else:
+            print(f"开始创建Github仓库: {dest_repo_name}...")
+        data = {
+            "name": dest_repo_name,
+            "private": private,
+        }
+
+        response = requests.post(cls.github_repos_create_url, headers=cls.headers, json=data)
+
+        if response.status_code == 201:
+            print(f"成功创建Github仓库: {dest_repo_name}")
+        else:
+            print(response.status_code)
+            print(f"创建Github仓库失败: {response.json()}")
+
+
+githubManager = GitHubRepoManager()
