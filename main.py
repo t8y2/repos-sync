0414775d7112cc01185c9d4ce2a312f0e6277c1b
skylertong <skyler@transcengram.com>
2024-10-05 00:54:32 +0800

    fix: 🐛 修复读取yaml位置错误

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from services.source_repos import SourceRepoManager
 
 if __name__ == "__main__":
-    manager = SourceRepoManager("../repos.yaml")
+    manager = SourceRepoManager("repos.yaml")
     manager.run()
 
