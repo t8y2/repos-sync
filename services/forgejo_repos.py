70f6e805bfb059785b76771fe6931984f964ab16
skylertong <skyler@transcengram.com>
2024-10-07 16:16:34 +0800

    feat: ✨  增加forgejo批量删除

--- services/forgejo_repos.py
+++ services/forgejo_repos.py
@@ -1,8 +1,5 @@
-import os
-
+import inquirer
 import requests
-from dotenv import load_dotenv
-
 from core.config import settings
 
 
@@ -14,6 +11,7 @@ class ForgejoManager:
             'Authorization': f'Bearer {cls.token}',
         }
         cls.list_repos_api = f'{settings["source"]["plat_host"]}/api/v1/user/repos'
+        cls.del_repos_api = f'{settings["source"]["plat_host"]}/api/v1/repos/'
 
     @classmethod
     def list_repos(cls) -> list:
@@ -24,5 +22,37 @@ class ForgejoManager:
             print(result.text)
             return []
 
+    def del_repos(cls) -> None:
+        results = cls.list_repos()
+        if len(results) == 0:
+            print("没有需要删除的仓库")
+            return
+        while True:
+            del_repos = inquirer.prompt([inquirer.Checkbox(
+                name="del_repo",
+                message="请选择将要删除的仓库",
+                choices=results,
+            )])['del_repo']
+            if len(del_repos) == 0:
+                print("请至少选择一个仓库")
+            confirm_del = inquirer.prompt([inquirer.Confirm(
+                name="confirm_del",
+                message=f"确认删除{del_repos}？",
+                default=True,
+            )])['confirm_del']
+            if not confirm_del:
+                print("取消删除, 返回仓库列表")
+            else:
+                for repo in del_repos:
+                    resp = requests.delete(cls.del_repos_api + repo, headers=cls.headers)
+                    if resp.status_code == 204:
+                        print(f"删除仓库{repo}成功")
+                    else:
+                        print(f'删除仓库{repo}失败: {resp.text}')
+                break
+
 
 forgejo = ForgejoManager()
+
+if __name__ == '__main__':
+    forgejo.del_repos()
